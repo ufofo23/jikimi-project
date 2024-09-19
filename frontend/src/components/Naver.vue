eb95ca2033fa9d2dd213b2376d91340e9953efda
zhyeon <pjh101124@gmail.com>
2024-09-19 15:25:36 +0900

    로그인 프론트엔드

---
+++ frontend/src/components/Naver.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="naver-login">
+    <button @click="naverLogin">
+      <img src="@/assets/naver.png" alt="Naver Login" class="naver-logo" />
+    </button>
+  </div>
+</template>
+
+<script setup>
+const naverLogin = () => {
+  // 네이버 로그인 로직
+};
+</script>
+
+<style scoped>
+.naver-logo {
+  width: 55px;
+  height: 55px;
+}
+button {
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+</style>
