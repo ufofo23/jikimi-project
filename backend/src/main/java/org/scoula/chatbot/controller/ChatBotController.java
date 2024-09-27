e78d206bb6941d7527b68967edaf0212273290f6
JaesungGo <102339979+JaesungGo@users.noreply.github.com>
2024-09-27 18:46:28 +0900

    Feat: Add ChatBot and Integrate authentication using jwt

---
+++ backend/src/main/java/org/scoula/chatbot/controller/ChatBotController.java
@@ -0,0 +1,24 @@
+package org.scoula.chatbot.controller;
+
+import org.scoula.chatbot.dto.ChatRequest;
+import org.scoula.chatbot.dto.ChatResponse;
+import org.scoula.chatbot.service.ChatBotService;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/chat")
+public class ChatBotController {
+
+    private final ChatBotService chatBotService;
+
+    public ChatBotController(ChatBotService chatBotService) {
+        this.chatBotService = chatBotService;
+    }
+
+    @PostMapping(produces = "application/json;charset=UTF-8", consumes = "application/json;charset=UTF-8")
+    public Mono<ChatResponse> chat(@RequestBody ChatRequest request) {
+        return chatBotService.getChatBotResponse(request.getPrompt())
+                .map(ChatResponse::new);
+    }
+}
