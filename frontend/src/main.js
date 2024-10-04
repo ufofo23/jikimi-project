1a29983b5f6bcad5f3a487a3aa9cb907feefbdf2
LAPTOP-U0CO45L6\user <chyoung000@naver.com>
2024-10-04 18:38:49 +0900

    Feat : Add DictionaryListPage liked-list

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,27 +5,25 @@ import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
-// import './assets/style.css'; // 커스텀 CSS 불러오기
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faCircleQuestion } from '@fortawesome/free-regular-svg-icons';
-import { faCircleQuestion as faSolidCircleQuestion } from '@fortawesome/free-solid-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons'; 
 import { far } from '@fortawesome/free-regular-svg-icons'; 
 
 
 
 // 필요한 아이콘을 라이브러리에 추가
-library.add(faCircleQuestion, faSolidCircleQuestion);
 library.add(fas, far);
 
-const app = createApp(App);
-const pinia = createPinia();
 
-app.use(pinia); // 이미 생성한 pinia 사용
+const app = createApp(App);
+
+// 아이콘 컴포넌트 등록
+app.component('font-awesome-icon', FontAwesomeIcon); 
+
+const pinia = createPinia();
+app.use(pinia);
 app.use(router);
 
 app.mount('#app');
-
-app.component('font-awesome-icon', FontAwesomeIcon);
