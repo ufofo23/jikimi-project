ee8080d245f2c445f630f422038d44d68c74268f
zhyeon <pjh101124@gmail.com>
2024-10-05 00:17:31 +0900

    Feat : Pagination  ( SenseList, PreventionList )

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,11 +6,16 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import './assets/style.css'; // 커스텀 CSS 불러오기
+import 'vue-awesome-paginate/dist/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './assets/tailwind.css'; // Tailwind CSS 추가
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faCircleQuestion } from '@fortawesome/free-regular-svg-icons';
 import { faCircleQuestion as faSolidCircleQuestion } from '@fortawesome/free-solid-svg-icons';
+import VueAwesomePaginate from 'vue-awesome-paginate';
 
 // 필요한 아이콘을 라이브러리에 추가
 library.add(faCircleQuestion, faSolidCircleQuestion);
@@ -18,6 +23,7 @@ library.add(faCircleQuestion, faSolidCircleQuestion);
 const app = createApp(App);
 const pinia = createPinia();
 
+app.use(VueAwesomePaginate);
 app.use(pinia); // 이미 생성한 pinia 사용
 app.use(router);
 
