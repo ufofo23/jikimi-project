2ff92d5f436d582581163900018e76bab29fd307
ufofo23 <ghdrhks0@gmail.com>
2024-10-07 13:30:58 +0900

    HOTFIX : duplicated importing in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,6 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons'; 
-import { far } from '@fortawesome/free-regular-svg-icons'; 
 // import './assets/style.css'; // 커스텀 CSS 불러오기
 import 'vue-awesome-paginate/dist/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -23,11 +19,10 @@ import { far } from '@fortawesome/free-regular-svg-icons';
 // 필요한 아이콘을 라이브러리에 추가
 library.add(fas, far);
 
-
 const app = createApp(App);
 
 // 아이콘 컴포넌트 등록
-app.component('font-awesome-icon', FontAwesomeIcon); 
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 const pinia = createPinia();
 app.use(VueAwesomePaginate);
