d011f5076097e1dbb8387ad81b078649e26c72db
ufofo23 <ghdrhks0@gmail.com>
2024-10-14 10:51:30 +0900

    Fix : map size

--- frontend/src/pages/map/MainMap.vue
+++ frontend/src/pages/map/MainMap.vue
@@ -544,9 +544,17 @@ onMounted(() => {
 </template>
 
 <style scoped>
+body {
+  margin: 0;
+  padding: 0;
+}
+
 .container {
   display: flex;
   height: 100vh;
+  max-width: 100vw;
+  margin: 0;
+  padding: 0;
 }
 
 .container div {
@@ -554,7 +562,7 @@ onMounted(() => {
 }
 
 .left-panel {
-  width: 30%;
+  width: 25%;
   padding: 20px;
   background-color: white;
   position: relative;
@@ -563,7 +571,7 @@ onMounted(() => {
 }
 
 .right-panel {
-  width: 70%;
+  width: 75%;
   position: relative;
   height: 90vh;
 }
